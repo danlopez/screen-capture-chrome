068baf909513ace9d0d87e9d8694e8bf72d1fa89
Dan Lopez <dlopez@instructure.com>
2018-04-10 15:29:14 -0400

    Chrome Extension to allow Screen Capture

---
+++ extension.js
@@ -0,0 +1,27 @@
+chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
+  const sources = message.sources;
+  const tab = sender.tab;
+
+  if (message.message === 'version') {
+    sendResponse({
+      type: 'version',
+      version: 1.0
+    });
+    return;
+  }
+
+  chrome.desktopCapture.chooseDesktopMedia(sources, tab, (streamId) => {
+    if (!streamId) {
+      sendResponse({
+        type: 'error',
+        message: 'Failed to get stream ID'
+      });
+    } else {
+      sendResponse({
+        type: 'success',
+        streamId: streamId
+      });
+    }
+  });
+  return true;
+});
